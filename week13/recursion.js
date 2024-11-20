ad52789c157d7fd9fb4925dece8fa116c80ce793
kuure <kuure@msu.edu>
2024-11-20 07:20:29 -0500

    added object canvas and constructor files

--- week13/recursion.js
+++ week13/recursion.js
@@ -30,13 +30,6 @@ console.log(factorialIterative(10));
 
 // a function to calcultate the factorial of an integer recursively
 function factorialRecursive(n) {
-
-	// Base case: factorial of 0 or 1 is 1
-	if (n === 0 || n === 1) {
-		return 1
-	}
-
-	// Recursive case
 	return n * factorialRecursive(n - 1)
 }
 
